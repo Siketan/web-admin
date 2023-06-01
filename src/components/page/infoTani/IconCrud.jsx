0d3d04d303b319bfb3c42ea11671c7ac9b6b4449
heri2610 <suhaeriheri45@gmail.com>
2023-06-01 12:23:00 +0700

    coba deploy

--- src/components/page/infoTani/IconCrud.jsx
+++ src/components/page/infoTani/IconCrud.jsx
@@ -2,9 +2,7 @@
 import MainCard from "../../MainCard"
 function IconCrud({icon, children}) {
   return (
-    <>
     <MainCard noPadding radius="0" gap="0" center style={{border:"solid 1px black"}} className="cursor-pointer">{icon}<span className="text-xs font-medium" style={{marginTop:-5}}>{children}</span></MainCard>
-    </>
   )
 }
 
