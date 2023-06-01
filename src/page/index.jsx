04ebed21b4c07fe37ce0e73277eefde8dbf7c9d3
heri2610 <suhaeriheri45@gmail.com>
2023-06-01 17:34:49 +0700

    coba deploy v2

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -15,6 +15,23 @@ import PresensiKehadiran from "./penyuluhanTani/detailPenyuluh/presensiKehadiran
 import TambahPenjual from "./tokoTani/tambahDataPenjual"
 import ProdukPenyuluh from "./tokoTani/produk/penyuluh"
 import ProdukPetani from "./tokoTani/produk/petani"
+import Airplane404 from "../assets/notFound.svg"
 
+const NotFoundPage = () => {
+  return (
+<div className="container img-banner">
+  <div className="flex justify-center mt-5 mb-5">
+    <div className="col-md-4">
+      <img src={Airplane404} alt="airplane404" />
+    </div>
+    <div className="col-md-4 text-center mt-5">
+      <h1 className="text-4xl font-bold text-blue-600">404</h1>
+      <h3 className="text-2xl text-gray-800">Page Not Found!</h3>
+    </div>
+  </div>
+</div>
 
-export {DataTani, TambahDataTani, LaporanPenyuluh, LaporanPetani, InfoTani, TambahInfoTani,EventTani, TambahEventTani, LiveChat, RatingPetugas, TambahPenyuluhanTani, DataRiwayatChat, JurnalKegiatan, PresensiKehadiran, TambahPenjual, ProdukPenyuluh, ProdukPetani}
+  );
+};
+
+export {DataTani, TambahDataTani, LaporanPenyuluh, LaporanPetani, InfoTani, TambahInfoTani,EventTani, TambahEventTani, LiveChat, RatingPetugas, TambahPenyuluhanTani, DataRiwayatChat, JurnalKegiatan, PresensiKehadiran, TambahPenjual, ProdukPenyuluh, ProdukPetani, NotFoundPage}
