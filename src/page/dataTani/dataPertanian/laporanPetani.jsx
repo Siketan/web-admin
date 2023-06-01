39bc8fa3af172ebdf141003a91f1b265bac451dc
afanfian <afanfian14@gmail.com>
2023-06-01 20:25:09 +0700

    feat: update styling data tani & toko tani

--- src/page/dataTani/dataPertanian/laporanPetani.jsx
+++ src/page/dataTani/dataPertanian/laporanPetani.jsx
@@ -124,7 +124,7 @@ function LaporanPetani() {
                             className="text-white bg-red-500 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-orange-800"
                         >
                             <FontAwesomeIcon icon={faCancel} className="mr-2" />
-                            Cancel
+                            Batalkan
                         </button>
                     </div>
                 </form>
