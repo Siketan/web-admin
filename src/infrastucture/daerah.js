eba0cfe87b4abead84b3a45aabb4e33ee053dbfc
heri2610 <suhaeriheri45@gmail.com>
2023-06-12 23:00:12 +0700

    kecamatan dan desa

---
+++ src/infrastucture/daerah.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+const fecthKecamatan = async()=>{
+    try {
+        const response = await axios.get('https://dev.farizdotid.com/api/daerahindonesia/kecamatan?id_kota=3521');
+        return response.data
+      } catch (error) {
+        return 'terjadi kesalahan'
+      }
+}
+const fecthDesa = async(id)=>{
+    try {
+        const response = await axios.get(`https://dev.farizdotid.com/api/daerahindonesia/kelurahan?id_kecamatan=${id}`);
+        return response.data
+      } catch (error) {
+        return 'terjadi kesalahan'
+      }
+}
+
+export {fecthKecamatan, fecthDesa}
