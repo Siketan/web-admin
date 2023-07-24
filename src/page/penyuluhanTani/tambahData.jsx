3ee9ffa780dde5f65b638d6da54715294f3164c5
heri2610 <suhaeriheri45@gmail.com>
2023-07-24 08:38:45 +0700

    merubah data desaBinaan

--- src/page/penyuluhanTani/tambahData.jsx
+++ src/page/penyuluhanTani/tambahData.jsx
@@ -84,7 +84,7 @@ const TambahPenyuluhanTani = ()=>{
                             <label htmlFor="nama" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"> <strong>Nama</strong> (Contoh: Subagyo Joyo Kumuso)</label>
                         </div>
                         <div className="relative z-0 w-full mb-6 group">
-                            <input type="password" name="password" id="password" value={password}  onChange={(e) => setPassword(e.target.value)} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                            <input type="password" name="password" id="password" value={password}  onChange={(e) => setPassword(e.target.value)} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                             <label htmlFor="password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"><strong>Password</strong></label>
                         </div>
                     </div>
@@ -152,7 +152,7 @@ const TambahPenyuluhanTani = ()=>{
                         </div>
                     </div>
                     <div className="relative z-0 w-[49%] mb-6 group">
-                        <input type="text" name="namaProduct" id="namaProduct" value={namaProduct}  onChange={(e) => setNamaProduct(e.target.value)} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <input type="text" name="namaProduct" id="namaProduct" value={namaProduct}  onChange={(e) => setNamaProduct(e.target.value)} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
                         <label htmlFor="namaProduct" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"><strong>Nama Produk</strong> (Contoh: SiKetan Hijau)</label>
                     </div>
                     <div className="flex space-x-4 justify-end">
