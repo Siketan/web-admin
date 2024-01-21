4951732b77efeaa43f19b98e80b54cdd2cdb036b
Aufi Fillah <fillahaufi140101@gmail.com>
2024-01-22 05:23:20 +0700

    fix: toko tani admin

--- src/@types/toko.d.ts
+++ src/@types/toko.d.ts
@@ -3,8 +3,34 @@ type TokoTani = {
 	namaProducts: string;
 	harga: number;
 	stok: number;
+	satuan: string;
 	fotoTanaman: string;
-	dataPerson: {
-		NoWa: string;
-	};
+	deskripsi: string;
+	status: string;
+	dataPerson: DataPerson;
+};
+
+type FilteredTokoTani = {
+	kecamatan: string;
+	desa: string;
+	NIK: string;
+	nama: string;
+	namaProducts: string;
+	stok: string;
+	satuan: string;
+	harga: string;
+	deskripsi: string;
+	fotoProduk: string;
+	status: string;
+};
+
+type DataPerson = {
+	NIK: string;
+	NIP: string;
+	NoWa: string;
+	alamat: string;
+	desa: string;
+	foto: string;
+	kecamatan: string;
+	nama: string;
 };
